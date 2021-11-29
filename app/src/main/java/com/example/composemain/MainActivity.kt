5fe9addb498d1fb3e3a589dda3633cf563481540
beselam <44022867+beselam@users.noreply.github.com>
2021-11-29 13:46:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/composemain/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.composemain
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composemain.ui.theme.ComposeMainTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeMainTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeMainTheme {
+        Greeting("Android")
+    }
+}
